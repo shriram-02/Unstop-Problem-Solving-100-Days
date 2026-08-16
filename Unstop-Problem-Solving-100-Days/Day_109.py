2ccc5d39adbc83c623168091651689dbeaa0f3f4
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-08-16 08:23:51 +0530

    Add files via upload

---
+++ Unstop-Problem-Solving-100-Days/Day_109.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+
+prefix = [0] * (n + 1)
+
+for i in range(n):
+    prefix[i + 1] = prefix[i] + a[i]
+
+q = int(input())
+
+for _ in range(q):
+    l, r = map(int, input().split())
+    total = prefix[r] - prefix[l - 1]
+
+    if total > 0:
+        status = "SURPLUS"
+    elif total < 0:
+        status = "DEFICIT"
+    else:
+        status = "BALANCED"
+
+    print(total, status)
